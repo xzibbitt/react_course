51c1883ba0ee0eaa5127e7591612a576572f8abf
xzibbitt <tyrawitesse@gmail.com>
2021-12-11 11:00:17 +0300

    commit2

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// const element = <h1 className="test">Hello World!</h1>;
+
+// const elementTwo = React.createElement(
+//   "h1",
+//   { className: "test" },
+//   "Hello, World!"
+// );
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// ReactDOM.render(elementTwo, document.getElementById("root"));
